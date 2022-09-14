eae6bc99975f7bf5450d345bc129a2606d383b4a
Tapio Meriläinen <tapiom4@gmail.com>
2022-09-14 14:02:22 +0300

    Project v 0.1

---
+++ app/src/main/java/com/example/tietokirjasovellus/listViewFragment.java
@@ -0,0 +1,19 @@
+package com.example.tietokirjasovellus;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.View;
+
+
+public class listViewFragment extends Fragment {
+    listViewFragment(){super(R.layout.fragment_list_view);}
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
